f576e482796d4089472988bd3a43fdba2521d1fa
mariannacoppa <densimetra@gmail.com>
2024-06-24 17:36:01 +0200

    [FEAT] create bonus folder and css style file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.getElementById("title").innerHTML = "Password Generator Wannabe";
 document.getElementById("subtitle").innerHTML = "<em>Crea una password inserendo nome, cognome e colore preferito</em>";
 // dichiarazione e assegnazione della variabile "nome"
-let nome = prompt("inserisci il tuo nome");
+let nome = prompt("Inserisci il tuo nome, tutto in minuscolo");
 // dichiarazione e assegnazione della variabile "cognome"
-let cognome = prompt("inserisci il tuo cognome");
+let cognome = prompt("Inserisci il tuo cognome, tutto in minuscolo");
 // dichiarazione e assegnazione della variabile "colorePreferito"
-let colorePreferito = prompt("inserisci il tuo colore preferito");
+let colorePreferito = prompt("Inserisci il tuo colore preferito, tutto in minuscolo");
 // concatenazione tra le 3 stringhe precedenti
 document.getElementById("template_literal").innerText = `${nome}${cognome}${colorePreferito}23`;
